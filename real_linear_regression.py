9edea3021b7a47884a69c97ee7b2f1daf523a87d
nkt <panins41@gmail.com>
2018-04-29 17:57:46 +0800

    pep8

--- real_linear_regression.py
+++ real_linear_regression.py
@@ -7,14 +7,14 @@ ops.reset_default_graph()
 sess = tf.Session()
 iris = datasets.load_iris()
 
-x_vals = np.array([x[3] for x in iris.data],dtype=np.float32)
+x_vals = np.array([x[3] for x in iris.data], dtype=np.float32)
 y_vals = np.array([y[0] for y in iris.data])
 
 
 learning_rate = 0.05
 batch_size = 25
-x_data = tf.placeholder(shape=[None,1], dtype=tf.float32)
-y_target = tf.placeholder(shape=[None,1], dtype=tf.float32)
+x_data = tf.placeholder(shape=[None, 1], dtype=tf.float32)
+y_target = tf.placeholder(shape=[None, 1], dtype=tf.float32)
 A = tf.Variable(tf.random_normal(shape=[1,1]))
 b = tf.Variable(tf.random_normal(shape=[1,1]))
 
